338befabc6d312a84aff99ee1e4f1cb8fabe2d86
paltech <nicole.caballero@paltech.eu>
2026-04-15 14:20:04 +0200

    add: redirect /

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
-
-
 import type {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/de',
+        permanent: false
+      }
+    ];
+  }
+};
 
 const withNextIntl = createNextIntlPlugin();
 
